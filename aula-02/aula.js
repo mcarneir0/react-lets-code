b49c5eddb2740d159ef51b1882cf6716bdff3afd
Matheus Carneiro <76215464+mcarneir0@users.noreply.github.com>
2022-03-30 22:09:59 -0300

    Adicionado Aula 02

---
+++ aula-02/aula.js
@@ -0,0 +1,10 @@
+//  Refatorar o código da forca adicionando comentários
+//  e classificando as funções
+
+// Stateless ou Statefull
+// Função independente
+// Funções Puras
+// Composição de Função
+// Estado Compartilhado
+// Funções de primeira classe
+// Funções de ordem superior
