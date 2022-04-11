6f0b24cf7803b7f504d4014d946769a33d541e16
Matheus Carneiro <76215464+mcarneir0@users.noreply.github.com>
2022-04-11 19:52:52 -0300

    Aula 06 - Criando Tabelas com React

--- aula-06/first-app/src/pages/Home/index.jsx
+++ aula-06/first-app/src/pages/Home/index.jsx
@@ -1,17 +1,59 @@
 import React from 'react'
-
 import CountProvider from '../../contexts/Count'
-
 import { Count } from '../../components/Count'
 import { MostrarCount } from '../../components/Count/mostrarCount'
+import { Table } from 'react-bootstrap'
 
 const Home = () => {
+
+  const usuarios = [{
+    id: 1,
+    nome: 'Mark',
+    sobrenome: 'Otto',
+    username: '@mdo'
+  }, {
+    id: 2,
+    nome: 'Jacob',
+    sobrenome: 'Thornton',
+    username: '@fat'
+  }, {
+    id: 3,
+    nome: 'Matheus',
+    sobrenome: 'Carneiro',
+    username: '@m.carneir0'
+  }];
+
+  const montarLista = (usuario) => {
+    return (
+      <tr key={usuario.id}>
+        <td>{usuario.id}</td>
+        <td>{usuario.nome}</td>
+        <td>{usuario.sobrenome}</td>
+        <td>{usuario.username}</td>
+      </tr>
+    );
+  }
+
   return <CountProvider>
     <Count/>
 
     <hr />
 
     <MostrarCount/>
+
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Nome</th>
+          <th>Sobrenome</th>
+          <th>Usuário</th>
+        </tr>
+      </thead>
+      <tbody>
+        {usuarios.map(usuario => montarLista(usuario))}
+      </tbody>
+    </Table>
   </CountProvider>
 }
 
